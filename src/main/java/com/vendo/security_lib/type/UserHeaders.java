5136a9d3aaf0854b383187f878637e538973625e
HaaaamBurger <gemasclashes@gmail.com>
2026-06-03 14:49:06 +0300

    kan-42: refactored names

--- src/main/java/com/vendo/security_lib/type/UserHeaders.java
+++ src/main/java/com/vendo/security_lib/type/UserHeaders.java
@@ -2,9 +2,9 @@ package com.vendo.security_lib.type;
 
 public enum UserHeaders {
 
-    USER_ID("X-User-Id"),
-    USER_EMAIL("X-User-Email"),
-    STATUS("X-User-Status"),
+    ID("X-Id"),
+    EMAIL("X-Email"),
+    STATUS("X-Status"),
     ROLES("X-Roles"),
     EMAIL_VERIFIED("X-Email-Verified");
 
